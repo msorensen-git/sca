a6e1c984791ceafade418ad068b0bad027c7c4f8
msorensen-git <65434878+msorensen-git@users.noreply.github.com>
2023-08-17 03:11:05 -0400

    updated zone class

--- scp.py
+++ scp.py
@@ -5,15 +5,33 @@ class Zone:
     def __init__(self):        
         self.status = False
         self._timeOn = 0
+        self.label = None
+        self.indicator = None
+        self.button = None
         
+    def label(self):
+        return self.label
+    
+    def indicator(self):
+        return self.indicator
+    
+    def button(self):
+        return self.button
+    
     def setOn(self):
         self.status = True
+        self.indicator.configure(bg="red")
             
     def setOff(self):
         self.status = False
+        self.indicator.configure(bg='Gray32')
     
     def toggle(self):
-        self.status = not self.status
+        # self.status = not self.status
+        if self.status:
+            self.setOff()
+        else:
+            self.setOn()
         
     def isOn(self):
         return self.status
@@ -31,79 +49,68 @@ class SprinklerControlPanelApp:
         self.root.title("Sprinkler Control Panel")        
 
         gridRow = 0
-        self.zonesLabel = []
-        self.zonesIndicator = []
-        self.zonesButton = []
-        self.zonesZone = []
+        self.zones = []
 
-        self.zonesLabel.append(tk.Label(root, text="Zone 1", padx=10))
-        self.zonesLabel[gridRow].grid(row=gridRow, column=0)
-        self.zonesIndicator.append(tk.Label(root, bg=self.offColor, width=2, height=1))
-        self.zonesIndicator[gridRow].grid(row=gridRow, column=1)
-        self.zonesButton.append(tk.Button(root, text="Toggle Z1", command=lambda: self.toggle_status(1)))
-        self.zonesButton[gridRow].grid(row=gridRow, column=2)
-        self.zonesZone.append(Zone())
-
-        gridRow += 1
-        self.zonesLabel.append(tk.Label(root, text="Zone 2", padx=10))
-        self.zonesLabel[gridRow].grid(row=gridRow, column=0)
-        self.zonesIndicator.append(tk.Label(root, bg=self.offColor, width=2, height=1))
-        self.zonesIndicator[gridRow].grid(row=gridRow, column=1)
-        self.zonesButton.append(tk.Button(root, text="Toggle Z2", command=lambda: self.toggle_status(2)))
-        self.zonesButton[gridRow].grid(row=gridRow, column=2)
-        self.zonesZone.append(Zone())
+        self.zones.append(Zone())
+        self.zones[gridRow].label = tk.Label(root, text="Zone 1", padx=10)
+        self.zones[gridRow].label.grid(row=gridRow, column=0)
+        self.zones[gridRow].indicator = tk.Label(root, width=2, height=1)
+        self.zones[gridRow].indicator.grid(row=gridRow, column=1)
+        self.zones[gridRow].button = tk.Button(root, text="Toggle Z1", command=lambda: self.toggle_status(1))
+        self.zones[gridRow].button.grid(row=gridRow, column=2)
+        self.zones[gridRow].setOff()
+        self.zones[gridRow].setTimeOn(1)
 
         gridRow += 1
-        self.zonesLabel.append(tk.Label(root, text="Zone 3", padx=10))
-        self.zonesLabel[gridRow].grid(row=gridRow, column=0)
-        self.zonesIndicator.append(tk.Label(root, bg=self.offColor, width=2, height=1))
-        self.zonesIndicator[gridRow].grid(row=gridRow, column=1)
-        self.zonesButton.append(tk.Button(root, text="Toggle Z3", command=lambda: self.toggle_status(3)))
-        self.zonesButton[gridRow].grid(row=gridRow, column=2)
-        self.zonesZone.append(Zone())
+        self.zones.append(Zone())
+        self.zones[gridRow].label = tk.Label(root, text="Zone 2", padx=10)
+        self.zones[gridRow].label.grid(row=gridRow, column=0)
+        self.zones[gridRow].indicator = tk.Label(root, bg=self.offColor, width=2, height=1)
+        self.zones[gridRow].indicator.grid(row=gridRow, column=1)
+        self.zones[gridRow].button = tk.Button(root, text="Toggle Z2", command=lambda: self.toggle_status(2))
+        self.zones[gridRow].button.grid(row=gridRow, column=2)
+        self.zones[gridRow].setOff()
+        self.zones[gridRow].setTimeOn(1)
 
         gridRow += 1
-        self.zonesLabel.append(tk.Label(root, text="Zone 4", padx=10))
-        self.zonesLabel[gridRow].grid(row=gridRow, column=0)
-        self.zonesIndicator.append(tk.Label(root, bg=self.offColor, width=2, height=1))
-        self.zonesIndicator[gridRow].grid(row=gridRow, column=1)
-        self.zonesButton.append(tk.Button(root, text="Toggle Z4", command=lambda: self.toggle_status(4)))
-        self.zonesButton[gridRow].grid(row=gridRow, column=2)
-        self.zonesZone.append(Zone())
+        self.zones.append(Zone())
+        self.zones[gridRow].label = tk.Label(root, text="Zone 3", padx=10)
+        self.zones[gridRow].label.grid(row=gridRow, column=0)
+        self.zones[gridRow].indicator = tk.Label(root, bg=self.offColor, width=2, height=1)
+        self.zones[gridRow].indicator.grid(row=gridRow, column=1)
+        self.zones[gridRow].button = tk.Button(root, text="Toggle Z3", command=lambda: self.toggle_status(3))
+        self.zones[gridRow].button.grid(row=gridRow, column=2)
+        self.zones[gridRow].setOff()
+        self.zones[gridRow].setTimeOn(1)
 
         gridRow += 1
-        self.zonesLabel.append(tk.Label(root, text="Zone 5", padx=10))
-        self.zonesLabel[gridRow].grid(row=gridRow, column=0)
-        self.zonesIndicator.append(tk.Label(root, bg=self.offColor, width=2, height=1))
-        self.zonesIndicator[gridRow].grid(row=gridRow, column=1)
-        self.zonesButton.append(tk.Button(root, text="Toggle Z5", command=lambda: self.toggle_status(5)))
-        self.zonesButton[gridRow].grid(row=gridRow, column=2)
-        self.zonesZone.append(Zone())
+        self.zones.append(Zone())
+        self.zones[gridRow].label = tk.Label(root, text="Zone 4", padx=10)
+        self.zones[gridRow].label.grid(row=gridRow, column=0)
+        self.zones[gridRow].indicator = tk.Label(root, bg=self.offColor, width=2, height=1)
+        self.zones[gridRow].indicator.grid(row=gridRow, column=1)
+        self.zones[gridRow].button = tk.Button(root, text="Toggle Z4", command=lambda: self.toggle_status(4))
+        self.zones[gridRow].button.grid(row=gridRow, column=2)
+        self.zones[gridRow].setOff()
+        self.zones[gridRow].setTimeOn(1)
 
-        self.zonesZone[0].setOff()
-        self.zonesZone[1].setOff()
-        self.zonesZone[2].setOff()
-        self.zonesZone[3].setOff()
-        self.zonesZone[4].setOff()
-
-        self.zonesZone[0].setTimeOn(1)
-        self.zonesZone[1].setTimeOn(1)
-        self.zonesZone[2].setTimeOn(1)
-        self.zonesZone[3].setTimeOn(1)
-        self.zonesZone[4].setTimeOn(1)
-
-        self.set_status(1, False)
-        self.set_status(2, False)
-        self.set_status(3, False)        
-        self.set_status(4, False)        
-        self.set_status(5, False)        
+        gridRow += 1
+        self.zones.append(Zone())
+        self.zones[gridRow].label = tk.Label(root, text="Zone 5", padx=10)
+        self.zones[gridRow].label.grid(row=gridRow, column=0)
+        self.zones[gridRow].indicator = tk.Label(root, bg=self.offColor, width=2, height=1)
+        self.zones[gridRow].indicator.grid(row=gridRow, column=1)
+        self.zones[gridRow].button = tk.Button(root, text="Toggle Z5", command=lambda: self.toggle_status(5))
+        self.zones[gridRow].button.grid(row=gridRow, column=2)
+        self.zones[gridRow].setOff()
+        self.zones[gridRow].setTimeOn(1)
 
         # Sliders
         gridRow += 1
         self.temp_slider = tk.Scale(root, from_=0, to=100, orient="horizontal", command=self.temp_slider_changed)
         self.temp_slider.grid(row=gridRow, column=2)
 
-        self.temp_slider_value_label = tk.Label(root, text="Temp: z F")
+        self.temp_slider_value_label = tk.Label(root, text="Temp z F")
         self.temp_slider_value_label.grid(row=gridRow, column=3)
         self.temp_slider.set(1)     # start at value 1
 
@@ -119,64 +126,60 @@ class SprinklerControlPanelApp:
         current_time = strftime('%H:%M:%S %p')
         self.time_label.config(text=current_time)
         self.elapsedTime += 1
-        # print(f"ET: {self.elapsedTime}  Z1: {self.zonesZone[0].timeOn()}  Z2: {self.zonesZone[1].timeOn()}  Z3: {self.zonesZone[2].timeOn()}  Z4: {self.zonesZone[3].timeOn()}  Z5: {self.zonesZone[4].timeOn()}")
+        # print(f"ET: {self.elapsedTime}  Z1: {self.zones[0].timeOn()}  Z2: {self.zones[1].timeOn()}  Z3: {self.zones[2].timeOn()}  Z4: {self.zones[3].timeOn()}  Z5: {self.zones[4].timeOn()}")
 
-        if (self.elapsedTime == 10):
-            self.zonesZone[0].setOn()
-            self.set_status(1, True)                                               
-            # print("zone 1")
-        elif (self.elapsedTime == 10 + self.zonesZone[0].timeOn()):
-            self.zonesZone[0].setOff()
-            self.set_status(1, False)
-            self.zonesZone[1].setOn()
-            self.set_status(2, True)
-            # print("zone 2")
-        elif (self.elapsedTime == 10 + self.zonesZone[0].timeOn() + self.zonesZone[1].timeOn()):
-            self.zonesZone[1].setOff()
-            self.set_status(2, False)
-            self.zonesZone[2].setOn()
-            self.set_status(3, True)
-            # print("zone 3")
-        elif (self.elapsedTime == 10 + self.zonesZone[0].timeOn() + self.zonesZone[1].timeOn() + self.zonesZone[2].timeOn()):
-            self.zonesZone[2].setOff()
-            self.set_status(3, False)
-            self.zonesZone[3].setOn()
-            self.set_status(4, True)
-            # print("zone 4")
-        elif (self.elapsedTime == 10 + self.zonesZone[0].timeOn() + self.zonesZone[1].timeOn() + self.zonesZone[2].timeOn() + self.zonesZone[3].timeOn()):
-            self.zonesZone[3].setOff()
-            self.set_status(4, False)
-            self.zonesZone[4].setOn()
-            self.set_status(5, True)
-            # print("zone 5")
-        elif (self.elapsedTime >= 10 + self.zonesZone[0].timeOn() + self.zonesZone[1].timeOn() + self.zonesZone[2].timeOn() + self.zonesZone[3].timeOn() + self.zonesZone[4].timeOn()):
-            self.zonesZone[4].setOff()
-            self.set_status(5, False)
+        timeZone1 = self.zones[0].timeOn()
+        timeZone2 = timeZone1 + self.zones[1].timeOn()
+        timeZone3 = timeZone2 + self.zones[2].timeOn()
+        timeZone4 = timeZone3 + self.zones[3].timeOn()
+        timeZone5 = timeZone4 + self.zones[4].timeOn()
+        
+        if self.elapsedTime >= 10 + timeZone5:
+            self.zones[4].setOff()
             self.elapsedTime = 0
             # print("zone 5 off")
+        elif self.elapsedTime >= 10 + timeZone4:
+            self.zones[3].setOff()
+            self.zones[4].setOn()
+            # print("zone 5")
+        elif self.elapsedTime >= 10 + timeZone3:
+            self.zones[2].setOff()
+            self.zones[3].setOn()
+            # print("zone 4")
+        elif self.elapsedTime >= 10 + timeZone2:
+            self.zones[1].setOff()
+            self.zones[2].setOn()
+            # print("zone 3")
+        elif self.elapsedTime >= 10 + timeZone1:
+            self.zones[0].setOff()
+            self.zones[1].setOn()
+            # print("zone 2")
+        elif self.elapsedTime >= 10:
+            self.zones[0].setOn()
+            # print("zone 1")
             
         self.time_label.after(100, self.update_time)  # Update every 1000ms (1 second)
 
     def temp_slider_changed(self, value):
         self.temp_slider_value_label.config(text=f"Temp: {value} F")
         v = int(value)
-        self.zonesZone[0].setTimeOn(v)
-        self.zonesZone[1].setTimeOn(v)
-        self.zonesZone[2].setTimeOn(v)
-        self.zonesZone[3].setTimeOn(v)
-        self.zonesZone[4].setTimeOn(v)
+        self.zones[0].setTimeOn(v)
+        self.zones[1].setTimeOn(v)
+        self.zones[2].setTimeOn(v)
+        self.zones[3].setTimeOn(v)
+        self.zones[4].setTimeOn(v)
 
     def set_status(self, zone, status):
             if status:
-                self.zonesIndicator[zone-1].configure(bg="red")
-                self.zonesZone[zone-1].setOn()
+                self.zones[zone-1].indicator.configure(bg="red")
+                self.zones[zone-1].setOn()
             else:
-                self.zonesIndicator[zone-1].configure(bg=self.offColor)
-                self.zonesZone[zone-1].setOff()
+                self.zones[zone-1].indicator.configure(bg=self.offColor)
+                self.zones[zone-1].setOff()
     
     def toggle_status(self, zone):
-        self.zonesZone[zone-1].toggle()
-        self.set_status(zone, self.zonesZone[zone-1].isOn())
+        self.zones[zone-1].toggle()
+        self.set_status(zone, self.zones[zone-1].isOn())
 
 # Create a tkinter window
 root = tk.Tk()
